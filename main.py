45d7d0ff9cfc716afd84ae5fe64ec0d0a2ff40d4
Thornton Munson <66638430+ThorntonTMunson@users.noreply.github.com>
2020-07-10 10:49:03 -0600

    dev timeline and latest version of main
    An updated timeline of progress and the latest iteration of the main.py file before moving on to the logic of how the cards will interact with each other.

--- main.py
+++ main.py
@@ -64,24 +64,29 @@ def draw_card(frame):
     return pop_card
 
 
-def initial_draw():
-    player_draw()
-    comp_draw()
+# def black jack initial_draw():
+#     player_draw()
+#     comp_draw()
 
 
 def comp_draw():
     comp_hand.append(draw_card(comp_player_frame))
 
 
-def comp_health():
-    health = 10
+def comp_play():
+    comp_play.append(draw_card(comp_field))
 
+
+def comp_deck_field():
+    comp_deck.append(draw_card(comp_deck))
 
 
 def player_draw():
     user_hand.append(draw_card(user_player_frame))
 
-    user_player_hp = 10
+
+def player_play():
+    user_play.append(draw_card(player_field))
 
 
 def new_match():
@@ -106,66 +111,83 @@ mainWindow.configure(background="gray")
 ##########################################################
 # main window row 0
 # create a frame within the window to hold the comp health label place holder
-comp_health_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="bisque")
+comp_health_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="bisque", width=200, height=300)
 # set base parameters for the frame made above.
 comp_health_frame.grid(row=0, column=0, sticky="ew", columnspan=1, rowspan=1)
 # create a label to hold the comp title later will become user name as an enemy name
 tkinter.Label(comp_health_frame, text="Enemy Health", background="bisque", fg="black").grid(row=0, column=0)
 # create a label to hold the comp health value and place within the grid of comp_health_frame
-tkinter.Label(comp_health_frame, textvariable=comp_health, background="bisque", fg="black").grid(row=1, column=0)
+tkinter.Label(comp_health_frame, textvariable=comp_health_frame, background="bisque", fg="black").grid(row=1, column=0)
 # comp_health_frame = tkinter.Frame(mainWindow, width=400, height=400, bg="bisque")
+
+################################################################################
+################################################################################
 # create a frame within the window to hold the comp deck image place holder
-comp_deck_frame = tkinter.Frame(mainWindow, relief="raised", borderwidth=1, background="bisque")
+comp_deck_frame = tkinter.Frame(mainWindow, relief="raised", borderwidth=10, background="bisque")
 comp_deck_frame.grid(row=0, column=1, sticky="ew", columnspan=1, rowspan=1)
-# card backing to the deck frame
-comp_deck_frame = tkinter.Frame(comp_deck_frame, bg="bisque")
+# load card backing to the deck frame
+comp_deck = tkinter.Frame(comp_deck_frame, background="bisque")
+comp_deck.grid(row=0, column=0)
+
 # frame for comp hand
 comp_hand_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="bisque")
 comp_hand_frame.grid(row=0, column=2, sticky="ew", columnspan=1, rowspan=5)
-
 # draws a card to comp player hand through the comp draw button
 comp_player_frame = tkinter.Frame(comp_hand_frame, background="bisque")
-comp_player_frame.grid(row=0, column=1, sticky="nsew", rowspan=1)
-# frame for comp discard pile
-comp_dis_pile = tkinter.Frame(mainWindow, relief="sunken", borderwidth=3, background="bisque")
-comp_dis_pile.grid(row=0, column=3, sticky="e", columnspan=1, rowspan=1)
-#########################################################
-# main window row 1
-comp_player_field = tkinter.Frame(mainWindow, relief="raised", borderwidth=5, background="magenta")
-comp_player_frame.grid(row=1, column=0, sticky="ew", rowspan=5)
-########################################################
-# main window row 3
+comp_player_frame.grid(row=0, column=1, sticky="ew", rowspan=1)
 
+# frame for comp discard pile
+comp_dis_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=3, background="bisque")
+comp_dis_frame.grid(row=0, column=3, sticky="e", columnspan=1, rowspan=1)
+# draw a card into the discard pile
+comp_dis_pile = tkinter.Frame(comp_dis_frame, background="bisque")
+comp_dis_pile.grid(row=0, column=0)
+########################################################################
+# main window row 1
+comp_field_frame = tkinter.Frame(mainWindow, relief="raised", borderwidth=10, background="magenta")
+comp_field_frame.grid(row=1, column=2, sticky="ew", rowspan=5)
+# draws a card to the comp player frame
+comp_field = tkinter.Frame(comp_field_frame, background="yellow")
+comp_field.grid(row=0, column=1, sticky="ew", rowspan=5)
+########################################################################
+########################################################################
+# main window row 2
+player_field = tkinter.Frame(mainWindow, relief="raised", borderwidth=10, bg="brown")
+player_field.grid(row=2, column=2, sticky="ew", rowspan=5)
+# draws a cards to the player play field
+player_field_play = tkinter.Frame(player_field, background="green")
+player_field_play.grid(row=0, column=0, sticky="ew", rowspan=5)
 #######################################################
-# main window row 4
+# main window row 3
 # get everything sorted for the comp set up and it's pretty much copypasta with a renaming of the variables
 # create a frame for player name and health
 player_health_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="yellow")
 # set base parameters of frame items below are within the player_health_frame
-player_health_frame.grid(row=1, column=0, sticky="ew", columnspan=1, rowspan=1)
+player_health_frame.grid(row=3, column=0, sticky="ew", columnspan=1, rowspan=1)
 # create a label to hold the player name
 tkinter.Label(player_health_frame, text="Player Health", background="yellow", fg="black").grid(row=1, column=0)
 # create a label to hold the player health value
 tkinter.Label(player_health_frame, textvariable=player_health_frame, background="brown", fg="blue").grid(row=2, column=0)
 # draw a card for player via the player draw button
-user_player_frame = tkinter.Frame(comp_deck_frame, background="green")
-user_player_frame.grid(row=3, column=1, sticky="ew", rowspan=2)
-
+player_frame = tkinter.Frame(comp_deck_frame, background="green")
+player_frame.grid(row=3, column=1, sticky="ew", rowspan=2)
+# create a frame to hold the players discard pile
+player_dis_pile = tkinter.Frame(mainWindow, relief="sunken", borderwidth=10, background="green")
 # for testing purposes to make sure things line ups do the copypasta early
 # draw cards into the user hand window
-user_hand_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="blue")
-user_hand_frame.grid(row=4, column=2, sticky="ew", columnspan=1, rowspan=5)
+player_hand_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="blue")
+player_hand_frame.grid(row=3, column=2, sticky="ew", columnspan=1, rowspan=5)
 # draws a card for the user player through the player draw button
-user_player_frame = tkinter.Frame(user_hand_frame, background="magenta")
-user_player_frame.grid(row=4, column=1, sticky="ew", rowspan=1)
+user_player_frame = tkinter.Frame(player_hand_frame, background="magenta")
+user_player_frame.grid(row=3, column=1, sticky="ew", rowspan=1)
 ########################################################
-# main window row 5
+# main window row 4
 
 # create frame for buttons within existing grid
 ########################################
 button_frame = tkinter.Frame(mainWindow)
 ########################################
-button_frame.grid(row=5, column=0, columnspan=3, sticky="w")
+button_frame.grid(row=4, column=0, columnspan=4, sticky="w")
 
 # create buttons and place in button frame
 comp_button = tkinter.Button(button_frame, text="Comp Player", command=comp_draw)
@@ -179,7 +201,21 @@ new_match_button.grid(row=0, column=2)
 
 shuffle_button = tkinter.Button(button_frame, text="Shuffle Cards", command=shuffle)
 shuffle_button.grid(row=0, column=3)
+################################################################################
+# for testing purposes
+comp_deck_button = tkinter.Button(button_frame, text="com deck", command=comp_deck_field)
+comp_deck_button.grid(row=1, column=0)
 
+comp_play = tkinter.Button(button_frame, text="comp play", command=comp_play)
+comp_play.grid(row=1, column=1)
+
+comp_dis_button = tkinter.Button(button_frame, text="comp discard", command=comp_dis_pile)
+comp_dis_button.grid(row=1, column=2)
+
+
+user_play = tkinter.Button(button_frame, text="player play", command=player_play)
+user_play.grid(row=2, column=0)
+################################################################################
 cards = []
 load_cards(cards)
 print(cards)
@@ -188,4 +224,9 @@ shuffle()
 
 user_hand = []
 comp_hand = []
+################################################################################
+# for testing purposes
+comp_field = []
+comp_deck = []
+################################################################################
 tkinter.mainloop()
